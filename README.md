31cdf2e29a5c036fdc2ebbf38694b5c16bba7f79
PedroDNRusso <pedrodnrusso@gmail.com>
2025-05-04 21:03:05 -0300

    home, cadastro

--- README.md
+++ README.md
@@ -51,10 +51,16 @@ Loja de venda de discos online, com carrinho e função de vender seus proprios 
 * [Canvas](https://wellifabio.github.io/canvas/)
 * [Cronograma](https://wellifabio.github.io/gantt/)
 
+## Wireframes
+
+![Wireframe01](./docs/Wireframe01.PNG)
+![Wireframe02](./docs/Wireframe02.PNG)
+
 ## Progresso de Desenvolvimento
 
 | Data  | Alterações |
 | ------------- |:-------------:|
 | 03/05/2025     | Começo do Projeto     |
+| 04/05/2025     | Desenvolvimento de Pagina Inicial e Cadastro (Front-End)     |
 
 ## ByPedroDuarte
---
+++ web/cadastro/script.js
@@ -0,0 +1,33 @@
+const main = document.querySelector('main');
+var discos = [];
+
+//Verificar se há algo no localStorage
+var carrinho = JSON.parse(localStorage.getItem('carrinho'));
+if (carrinho == null) {
+    carrinho = [];
+}
+
+//Carregar os dados do arquivo JSON
+fetch('/api/assets/disco.json')
+    .then(response => response.json())
+    .then(data => {
+        discos = data;
+    }).
+    then(() => {
+        exibirCards();
+    });
+
+function exibirCards() {
+    discos.forEach((disco) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <img src="${disco.imagem}" alt="${disco.nome}">
+            <h2>${disco.nome}</h2>
+            <p>${disco.artista}</p>
+            <p>${disco.ano}</p>
+            <p>R$${disco.preco.toFixed(2)}</p>
+        `;
+        main.appendChild(card);
+    });
+}

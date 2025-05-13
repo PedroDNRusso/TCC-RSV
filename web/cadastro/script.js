1681699faa4d2652f48e2649b2f3eac2f56659b1
PedroDNRusso <pedrodnrusso@gmail.com>
2025-05-13 00:44:19 -0300

    cadastro html

--- web/cadastro/script.js
+++ web/cadastro/script.js
@@ -1,3 +1,4 @@
+const uri = 'http://localhost:3000';
 const main = document.querySelector('main');
 var discos = [];
 
@@ -23,4 +24,29 @@ function exibirCards() {
         `;
         main.appendChild(card);
     });
+}
+
+function cadastrar(){
+const form = document.querySelector('#cadastro form')
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const dados = {
+        nome: form.nome.value,
+        email: form.email.value,
+        senha: form.senha.value,
+    }
+    fetch('http://localhost:3000/cadastro', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dados)
+    })
+        .then(resp => resp.status)
+        .then(resp => {
+            if (resp == 201)
+                alert('Cadastro feito com sucesso!');
+            else
+                alert('Erro ao cadastrar!');
+                window.location.reload();
+        })
+})
 }
